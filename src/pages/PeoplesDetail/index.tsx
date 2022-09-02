9f08577ab5270d42e2e63fc358600384d310952d
SandroSena <sandro340@hotmail.com>
2022-09-01 22:44:16 -0300

    Thingsss

--- src/pages/PeoplesDetail/index.tsx
+++ src/pages/PeoplesDetail/index.tsx
@@ -1,19 +1,23 @@
 import { Typography } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { peoplesList, PeopleType } from '../Peoples';
+import { PeopleContainer } from './styles';
 
 const PeoplesDetail = () => {
   const { id } = useParams();
 
+  // const people: PeopleType | undefined = peoplesList.find((p) => p.id === id);
   const people: PeopleType | undefined = peoplesList.find((p) => p.id === id);
 
   console.log(id);
 
   return (
     <>
-      <Typography variant='h3'>People Details</Typography>
-      <Typography variant='h4'>{`Name: ${people?.name}`}</Typography>
-      <Typography variant='h4'>{`Age: ${people?.age}`}</Typography>
+      <Typography variant='h3' align='center'>People Details</Typography>
+      <PeopleContainer>
+        <Typography variant='h4'>{`Name: ${people?.name}`}</Typography>
+        <Typography variant='h4'>{`Age: ${people?.age}`}</Typography>
+      </PeopleContainer>
     </>
   );
 };
